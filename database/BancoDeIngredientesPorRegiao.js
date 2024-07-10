4e71132466b56625a10e1bcd3d89b8114c41ed5f
leticiaSouzaDev <lec667121@gmail.com>
2024-07-10 19:17:10 -0300

    feat: organizando e adicionando banco de dados para aplicação

---
+++ database/BancoDeIngredientesPorRegiao.js
@@ -0,0 +1,39 @@
+const ingredientesPorRegiao = [
+    {
+        regiao: "Sudeste",
+        ingredientes: [
+            "Abobrinha", "Tomate", "Couve", "Alface", "Cenoura", 
+            "Beterraba", "Batata-doce", "Morango", "Maçã", "Pera", "Uva"
+        ]
+    },
+    {
+        regiao: "Sul",
+        ingredientes: [
+            "Laranja", "Limão", "Acelga", "Espinafre", "Milho", 
+            "Feijão", "Ervilha", "Cebolinha", "Salsa", "Coentro"
+        ]
+    },
+    {
+        regiao: "Nordeste",
+        ingredientes: [
+            "Maracujá", "Manga", "Mamão", "Caju", "Coco", 
+            "Pimentão", "Mandioca", "Melancia", "Banana", "Abacaxi"
+        ]
+    },
+    {
+        regiao: "Centro-Oeste",
+        ingredientes: [
+            "Pequi", "Guariroba", "Mandioca", "Peixes de água doce", 
+            "Milho", "Soja", "Arroz", "Feijão", "Carne de gado"
+        ]
+    },
+    {
+        regiao: "Norte",
+        ingredientes: [
+            "Açaí", "Cupuaçu", "Tucumã", "Castanha-do-Pará", 
+            "Pirarucu", "Jambu", "Pupunha", "Frutas tropicais"
+        ]
+    }
+];
+
+export { ingredientesPorRegiao };
